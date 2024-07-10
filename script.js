10c6eb440f96e06123331bb507f7b322c68ed2e2
Dharshana2610 <dhars2916@gmail.com>
2024-07-10 15:32:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//math.radom 0 to 1
+var random=Math.floor(Math.random()*100)+1;
+var attempt=0;
+document.getElementById('btn').addEventListener('click',function(){
+    attempt++;
+    var guess=parseInt(document.getElementById('guessinput').value);
+    if(guess==random){
+        display("Congratulation! You got it in attempt-"+attempt);
+        document.getElementById('btn').disabled=true;
+    }
+    else if(guess<random){
+        display("Too low!,don't give up")
+    }
+    else if(guess>random){
+        display("Too high!,be calm and think");
+    }
+});
+function display(message){
+    document.getElementById("msg").textContent=message;
+}
